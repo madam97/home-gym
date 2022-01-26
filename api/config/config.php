ddbece61b83c1a5dc5c7dda64d15b542f38c9838
Mézes Ádám <mezesadam1997@gmail.com>
2022-01-26 11:15:10 +0100

    added backend api base files

---
+++ api/config/config.php
@@ -0,0 +1,4 @@
+<?php
+
+define('ROOT', __DIR__ .'/../');
+define('DB_FILE', realpath(ROOT.'../data/db.json'));
---
+++ api/index.php
@@ -0,0 +1,18 @@
+<?php
+
+require './config/config.php';
+require './functions/array_sort.php';
+require './classes/api.php';
+require './classes/db.php';
+
+$api = new API();
+
+try {
+  DB::load();
+
+  $api->processRequest();
+
+  throw new \Exception('unknown endpoint');  
+} catch (\Exception $e) {
+  $api->sendError($e->getMessage(), $e->getCode());
+}
