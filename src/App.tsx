88d3407aaffffe69e972be9e5794a1a81e643c02
Mézes Ádám <mezesadam1997@gmail.com>
2022-02-15 18:25:42 +0100

    added REACT_APP_HOMEPAGE env variable changed website's header does not appear on admin interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,38 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, RouteComponentProps, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import routes from './config/routes';
-import { ProvideTheme } from './hooks/useTheme';
-import { ProvideAuth } from './hooks/useAuth';
 
 const App = (): JSX.Element => {
-  return (
-    <ProvideTheme>
-      <ProvideAuth>
-        <Router basename="/home-gym">
-          <div className="App">
-            <Header />
 
-            <main>
-              <Switch>
-                {routes.map((route, index): JSX.Element => (
-                  <Route 
-                    key={index}
-                    path={route.path}
-                    exact={route.exact}
-                  >
-                    <route.component
-                      {...route.props}
-                    />
-                  </Route>
-                ))}
-                
-                <Redirect to="/404" />
-              </Switch>
-            </main>
-          </div>
-        </Router>
-      </ProvideAuth>
-    </ProvideTheme>
+  const location = useLocation();
+
+  // --------------------------------
+
+  const isAdmin = /^\/admin.*/.test(location.pathname);
+
+  return (
+    <div className="App">
+      {!isAdmin && <Header />}
+
+      <main>
+        <Switch>
+          {routes.map((route, index): JSX.Element => (
+            <Route 
+              key={index}
+              path={route.path}
+              exact={route.exact}
+            >
+              <route.component
+                {...route.props}
+              />
+            </Route>
+          ))}
+          
+          <Redirect to="/404" />
+        </Switch>
+      </main>
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './style.scss';
 import App from './App';
+import { ProvideTheme } from './hooks/useTheme';
+import { ProvideAuth } from './hooks/useAuth';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ProvideTheme>
+      <ProvideAuth>
+        <Router basename={process.env.REACT_APP_HOMEPAGE}>
+          <App />
+        </Router>
+      </ProvideAuth>
+    </ProvideTheme>
   </React.StrictMode>,
   document.getElementById('root')
 );
